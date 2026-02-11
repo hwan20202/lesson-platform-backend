b7e1127550c5d766888627f33c3d6ca73d62e430
Jeonghwan Kim <51739985+hwan20202@users.noreply.github.com>
2026-02-11 23:34:39 +0900

    feat : TossPaymentClient cancel 메소드 구현

--- src/main/java/com/kosa/fillinv/payment/client/TossPaymentClient.java
+++ src/main/java/com/kosa/fillinv/payment/client/TossPaymentClient.java
@@ -13,18 +13,56 @@ import org.springframework.web.client.RestClient;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Comparator;
 
 @Component
 @RequiredArgsConstructor
 public class TossPaymentClient {
 
+    private static final int MAX_RETRY_COUNT = 2;
     private final RestClient tossRestClient;
     private final ObjectMapper objectMapper;
-
     private final String uri = "/v1/payments/confirm";
-    private static final int MAX_RETRY_COUNT = 2;
+
     public RefundExecutionResult cancel(PaymentRefundCommand command) {
-        return null;
+        int attempt = 0;
+
+        while (true) {
+            try {
+                TossPaymentConfirmationResponse response =
+                        tossRestClient.post()
+                                .uri(uriBuilder ->
+                                        uriBuilder
+                                                .path("/v1/payments/{paymentKey}/cancel")
+                                                .build(command.paymentKey()))
+                                .header("Idempotency-Key", command.paymentKey())
+                                .body(new TossPaymentCancelRequest(
+                                        command.cancelReason(),
+                                        command.refundAmount()
+                                ))
+                                .retrieve()
+                                .onStatus(
+                                        status -> status.is4xxClientError() || status.is5xxServerError(),
+                                        tossErrorHandler()
+                                )
+                                .body(TossPaymentConfirmationResponse.class);
+
+                return getRefundExecutionResult(response);
+
+            } catch (PSPConfirmationException e) {
+                attempt++;
+                if (!e.isRetryable() || attempt > MAX_RETRY_COUNT) {
+                    throw e;
+                }
+                backoff(attempt);
+            } catch (ResourceAccessException e) { // timeout / network
+                attempt++;
+                if (attempt > MAX_RETRY_COUNT) {
+                    throw e;
+                }
+                backoff(attempt);
+            }
+        }
     }
 
     public PaymentExecutionResult confirm(PaymentConfirmCommand command) {
@@ -44,63 +82,90 @@ public class TossPaymentClient {
                                 .retrieve()
                                 .onStatus(
                                         status -> status.is4xxClientError() || status.is5xxServerError(),
-                                        (req, res) -> {
-                                            try (InputStream is = res.getBody()) {
-                                                TossPaymentConfirmationResponse.TossFailureResponse errorResponse =
-                                                        objectMapper.readValue(is, TossPaymentConfirmationResponse.TossFailureResponse.class);
-
-                                                TossPaymentError tossPaymentError = TossPaymentError.get(errorResponse.code());
-
-                                                throw PSPConfirmationException.builder()
-                                                        .errorCode(tossPaymentError.getStatusCode().toString())
-                                                        .errorMessage(tossPaymentError.getDescription())
-                                                        .isSuccess(tossPaymentError.isSuccess())
-                                                        .isFailure(tossPaymentError.isFailure())
-                                                        .isUnknown(tossPaymentError.isUnknown())
-                                                        .isRetryable(tossPaymentError.isRetryable())
-                                                        .build();
-
-                                            } catch (IOException e) {
-                                                throw new RuntimeException("에러 응답 파싱 실패", e);
-                                            }
-                                        }
+                                        tossErrorHandler()
                                 )
                                 .body(TossPaymentConfirmationResponse.class);
 
-                return new PaymentExecutionResult(
-                        response.paymentKey(),
-                        response.orderId(),
-                        new PaymentExtraDetails(
-                                PaymentType.get(response.type().name()),
-                                PaymentMethod.get(response.method()),
-                                response.approvedAt().toInstant(),
-                                response.orderName(),
-                                PSPConfirmationStatus.get(response.status().name()),
-                                response.totalAmount().longValue(),
-                                response.toString()
-                        )
-                );
+                return getPaymentExecutionResult(response);
 
             } catch (PSPConfirmationException e) {
                 attempt++;
-
                 if (!e.isRetryable() || attempt > MAX_RETRY_COUNT) {
                     throw e;
                 }
-
                 backoff(attempt);
-
             } catch (ResourceAccessException e) { // timeout / network
                 attempt++;
-
                 if (attempt > MAX_RETRY_COUNT) {
                     throw e;
                 }
-
                 backoff(attempt);
             }
         }
     }
+
+
+    private static PaymentExecutionResult getPaymentExecutionResult(TossPaymentConfirmationResponse response) {
+        return new PaymentExecutionResult(
+                response.paymentKey(),
+                response.orderId(),
+                new PaymentExtraDetails(
+                        PaymentType.get(response.type().name()),
+                        PaymentMethod.get(response.method()),
+                        response.approvedAt().toInstant(),
+                        response.orderName(),
+                        PSPConfirmationStatus.get(response.status().name()),
+                        response.totalAmount().longValue(),
+                        response.toString()
+                )
+        );
+    }
+
+    private static RefundExecutionResult getRefundExecutionResult(TossPaymentConfirmationResponse response) {
+
+        // 하나의 결제 이벤트에 대해서 여러 취소 이벤트가 있을 수 있기 떄문에 리스트 형태
+        // 가장 최신의 데이터를 선택
+        TossPaymentConfirmationResponse.Cancel cancelEvent =
+                response.cancels().stream()
+                        .max(Comparator.comparing(TossPaymentConfirmationResponse.Cancel::canceledAt))
+                        .orElseThrow(); // 결제 취소가 성공한 경우 최소 1개 이상의 cancel 데이터가 존재하야함
+
+        return new RefundExecutionResult(
+                response.paymentKey(),
+                response.orderId(),
+                new RefundExtraDetails(
+                        cancelEvent.canceledAt().toInstant(),
+                        cancelEvent.cancelAmount().intValueExact(),
+                        cancelEvent.cancelReason(),
+                        cancelEvent.transactionKey(),
+                        response.toString()
+                )
+        );
+    }
+
+    private RestClient.ResponseSpec.ErrorHandler tossErrorHandler() {
+        return (req, res) -> {
+            try (InputStream is = res.getBody()) {
+                TossPaymentConfirmationResponse.TossFailureResponse errorResponse =
+                        objectMapper.readValue(is, TossPaymentConfirmationResponse.TossFailureResponse.class);
+
+                TossPaymentError tossPaymentError = TossPaymentError.get(errorResponse.code());
+
+                throw PSPConfirmationException.builder()
+                        .errorCode(tossPaymentError.getStatusCode().toString())
+                        .errorMessage(tossPaymentError.getDescription())
+                        .isSuccess(tossPaymentError.isSuccess())
+                        .isFailure(tossPaymentError.isFailure())
+                        .isUnknown(tossPaymentError.isUnknown())
+                        .isRetryable(tossPaymentError.isRetryable())
+                        .build();
+
+            } catch (IOException e) {
+                throw new RuntimeException("에러 응답 파싱 실패", e);
+            }
+        };
+    }
+
 
     private void backoff(int attempt) {
         try {
